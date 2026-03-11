a6dcdb05957499c764c250b8f71845ff081c653d
suporte.ti <kevin.calantone@DOMAIN.SIVA.COM>
2026-03-11 16:59:30 -0300

    Listando os filmes

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react"
 import api from '../../services/api'
+import { Link } from "react-router-dom"
+import './home.css'
 
-//URL DA API: https://api.themoviedb.org/3/movie/popular?api_key=034ab4707e90adc0815559e5687082e1&language=pt-BR
+//URL DA API: https://api.themoviedb.org/3/movie/popular?api_key=5669892fa7499bf0695df928b9511546&language=pt-BR
 
 function Home(){
     const [filmes, setFilmes] = useState([])
@@ -16,15 +18,30 @@ function Home(){
                 }
             })
 
-            console.log(response.data)
+           // console.log(response.data.results.slice(0,10))
+           setFilmes((response.data.results.slice(0,10)))
         }
 
         loadFilmes()
     },[])
    
     return(
-        <div>
-            <h1>Bem vindo a Home</h1>
+        <div className="container">
+            
+            <div className="lista-filmes">
+              
+              {filmes.map((filmes) =>{
+                return(
+                    <article key={filmes.id}>
+                        <strong>{filmes.title}</strong>
+                        <img src={`https://image.tmdb.org/t/p/original/${filmes.poster_path}`} alt="{filme.title" />
+                         <Link to={`/filmes/${filmes.id}`}>Acessar</Link>
+                    </article>
+                )
+              })}  
+            
+            </div>
+            
         </div>
     )
 }
