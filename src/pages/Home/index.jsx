20790eb97cd5fcb03b24f17cfa99eb1b226a34a0
Kevin Domingues <127136094+kevincalantone@users.noreply.github.com>
2026-03-13 01:53:12 -0300

    Adiciona tela de carregamento e tratamento de erro 404

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -7,6 +7,7 @@ import './home.css'
 
 function Home(){
     const [filmes, setFilmes] = useState([])
+    const [loading, setLoading] = useState(true)
    
     useEffect(() =>{
         async function loadFilmes() {
@@ -17,19 +18,25 @@ function Home(){
                     page: 1,
                 }
             })
-
-           // console.log(response.data.results.slice(0,10))
+            // console.log(response.data.results.slice(0,10))
            setFilmes((response.data.results.slice(0,10)))
+           setLoading(false)
         }
 
         loadFilmes()
     },[])
-   
+
+    if(loading){
+        return(
+            <div className="loading">
+                <h2>Carregando filmes...</h2>
+            </div>
+        )
+    }    
+    
     return(
         <div className="container">
-            
             <div className="lista-filmes">
-              
               {filmes.map((filmes) =>{
                 return(
                     <article key={filmes.id}>
