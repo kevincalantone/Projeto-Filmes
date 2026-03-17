89a09bec7046c8e0ca76069f60f3051ebc66d2ef
Kevin Domingues <127136094+kevincalantone@users.noreply.github.com>
2026-03-17 02:03:52 -0300

    Ajustando a cor de fundo

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,7 +5,7 @@ import {Link} from 'react-router-dom'
 function Header(){
     return(
        <header>
-            <Link className='logo' to="/">Prime FLix</Link>
+            <Link className='logo' to="/">Prime Flix</Link>
             <Link className='favoritos' to="/favoritos">Meus Filmes</Link>
        </header>
     )
