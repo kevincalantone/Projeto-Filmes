fb55ca5a671e68208989ff4764ba6b7b9a564612
Kevin Domingues <127136094+kevincalantone@users.noreply.github.com>
2026-03-18 01:00:30 -0300

    Salvando os filmes

--- src/pages/Filme/index.jsx
+++ src/pages/Filme/index.jsx
@@ -36,6 +36,26 @@ function Filme(){
             console.log("COMPONENTE FOI DESMONTADO")
         }
      },[navigate, id])
+
+     function salvarFilme(){
+        const minhaLista = localStorage.getItem("@primeFlix")
+        let filmesSalvos = JSON.parse(minhaLista) || []
+
+        const hasFilme = filmesSalvos.some(
+            (filmesSalvos) => filmesSalvos.id === filme.id
+        )
+
+        if(hasFilme){
+            alert("ESSE FILME JÁ ESTÁ NA LISTA")
+        }
+
+        filmesSalvos.push(filme)
+        localStorage.setItem("@primeFlix", JSON.stringify(filmesSalvos))
+        alert("FILMES SALVOS COM SUCESSO")
+     }
+
+
+
      if(loading){
         return(
             <div className="filme-info">
@@ -54,7 +74,7 @@ function Filme(){
             <strong>Avaliação: {filme.vote_average} / 10 </strong>
 
             <div className="area-buttons">
-                <button>Salvar</button>
+                <button onClick={salvarFilme}>Salvar</button>
                 <button>
                     <a target='_blank' rel='axtarnal' href={`https://youtube.com/results?search_query=${filme.title} Trailer`}>
                         Trailer</a>
